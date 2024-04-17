465bfc376696ed78e1a9a149f0a60bfe4d7e47ce
JonyRiverosF <jo.riverosf@duocuc.cl>
2024-04-16 23:51:22 -0400

    avance

--- src/app/Pages/registro-user/registro-user.page.ts
+++ src/app/Pages/registro-user/registro-user.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro-user',
@@ -8,16 +9,221 @@ import { Router } from '@angular/router';
 })
 export class RegistroUserPage implements OnInit {
 
-  constructor(private router: Router) { }
+  //Variables con el ngModel
+  nombre:string="";
+  apellido:string="";
+  rut:string="";
+  digitoVerificador:string="";
+  horario1:string="";
+  horario2:string="";
+
+
+  //Mensajes
+  mensajeName: string = "";
+  mensajeNameN: string = "";
+  mensajeNameE: string = "";
+
+  mensajeApellido: string = "";
+  mensajeApellidoN: string = "";
+  mensajeApellidoE: string = "";
+
+  mensajeRut: string = "";
+  mensajeDigitoVerificador: string = "";
+  mensajeHorario: string = "";
+  mensajeHorario1: string = "";
+
+
+  colorItemName: string = "light";
+  colorItemApellido: string = "light";
+  colorItemRut: string = "light";
+  colorItemDigitoVerificador: string = "light";
+  colorItemHorario: string = "light";
+  flag:boolean=true;
+
+  constructor(private router: Router,private alertController: AlertController) { }
 
   ngOnInit() {
   }
 
+  verificarRegistro(nombre: string, apellido: string, rut: string, digitoVerificador: string, horario1: string, horario2: string) {
+    //Cada vez que se presione el botón, los mensajes de declararán vacias
+    this.mensajeName = "";
+    this.mensajeNameN = "";
+    this.mensajeNameE = "";
+
+    this.mensajeApellido = "";
+    this.mensajeApellidoE="";
+    this.mensajeApellidoN="";
+
+    this.mensajeRut = "";
+
+    this.mensajeDigitoVerificador = "";
+
+    this.mensajeHorario = "";
+
+    this.colorItemApellido = "light";
+    this.colorItemName = "light";
+    this.colorItemRut = "light";
+    this.colorItemDigitoVerificador = "light";
+    this.colorItemHorario = "light";
+    
+    let flag = true;
+
+     // VALIDACIÓN DEL NOMBRE
+     if (nombre.trim() === "") {
+      this.mensajeName = "El nombre no puede estar vacío";
+      this.colorItemName = "danger";
+      flag = false;
+    } 
+    if (this.tieneNumeros(nombre)) {
+      this.mensajeNameN = "El nombre no debe contener números";
+      this.colorItemName = "danger";
+      flag = false;
+    }
+    if (this.tieneCaracterEspecial(nombre)) {
+        this.mensajeNameE = "El nombre no debe contener caracteres especiales";
+        this.colorItemName = "danger";
+        flag = false;
+    }
+    
+
+    // VALIDACIÓN DEL APELLIDO
+    if (apellido.trim() === "") {
+      this.mensajeApellido = "El apellido no puede estar vacío";
+      this.colorItemApellido = "danger";
+      flag = false;
+    } 
+    if (this.tieneNumerosA(apellido)) {
+      this.mensajeApellidoN = "El apellido no debe contener números";
+      this.colorItemApellido = "danger";
+      flag = false;
+    }
+    if (this.tieneCaracterEspecialA(apellido)) {
+      this.mensajeApellidoE = "El apellido no debe contener caracteres especiales";
+      this.colorItemApellido = "danger";
+      flag = false;
+    }
+    
+
+    // VALIDACIÓN DEL RUT
+    if (!this.validarRut(rut)) {
+      this.mensajeRut = "El RUT debe tener exactamente 8 dígitos";
+      this.colorItemRut = "danger";
+      flag = false;
+    }
+
+
+    // VALIDACIÓN DEL DÍGITO VERIFICADOR
+    if (digitoVerificador.trim() === "") {
+      this.mensajeDigitoVerificador = "El digito verificador no puede estar vacio";
+      this.colorItemDigitoVerificador = "danger";
+      flag = false;
+    }
+
+    if(this.calcularVerificador(rut)!=digitoVerificador){
+      this.mensajeDigitoVerificador="Digito verificador incorrecto"
+    }
+
+    // VALIDACIÓN DE LOS HORARIOS
+    if (horario1.trim() === "" && horario2.trim() === "") {
+      this.mensajeHorario = "Seleccione al menos un horario";
+      this.colorItemHorario = "danger";
+      flag = false;
+      
+    }
+    console.log(horario1);
+
+    // REDIRECCIÓN A LOS PLANES DEL GIMNASIO
+    if (flag) {
+      this.flag =true;
+      this.presentAlert("La barra roja le indicará la seguridad de su contraseña");
+    }
+}
+
+
+//Nombre Errores
+tieneNumeros(nombre: string): boolean {
+  if(nombre==""){
+    return true;
+  }
+  return /\d/.test(nombre); 
+}
+
+tieneCaracterEspecial(nombre: string): boolean {
+  if(nombre==""){
+    return true;
+  }
+  return /[!@#$%^&*-_(),.?":{}|<>]/.test(nombre);
+}
+
+//Apellido Errores
+tieneNumerosA(apellido: string): boolean {
+  if(apellido==""){
+    return true;
+  }
+  return /\d/.test(apellido); 
+}
+
+tieneCaracterEspecialA(apellido: string): boolean {
+  if(apellido==""){
+    return true;
+  }
+  return /[!@#$%^&*-_(),.?":{}|<>]/.test(apellido);
+}
+
+//Rut
+validarRut(rut: string): boolean {
+  // Expresión regular que verifica que el RUT tenga exactamente 8 dígitos
+  const regex = /^\d{8}$/;
+  return regex.test(rut);
+}
+
+//DV
+calcularVerificador(rut:string) {
+  rut = rut.toString();
+  let sum = 0;
+  let mul = 2;
+
+  let i = rut.length;
+  while (i--) {
+    sum = sum + parseInt(rut.charAt(i)) * mul;
+    if (mul % 7 === 0) {
+      mul = 2;
+    } else {
+      mul++;
+    }
+  }
+
+  const res = sum % 11;
+
+  if (res === 0) {
+    console.log(res);
+    return '0';
+  } else if (res === 1) {
+    console.log(res);
+    return 'k';
+  }
+
+  const resto = 11 - res;
+  const dv = resto.toString();
+  console.log(dv);
+  return dv;
+}
 
 
 
   
 
+
+async presentAlert(mensaje:string) {
+  const alert = await this.alertController.create({
+    header: 'Alerta',
+    subHeader: 'Seguridad de la contraseña',
+    message: mensaje,
+    buttons: ['OK'],
+  });
+  await alert.present();
+}
 
 
   irPerfil(){
