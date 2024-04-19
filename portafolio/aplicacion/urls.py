06eaaef35a1e31457921529e677c7084507c5fa7
JonyRiverosF <jo.riverosf@duocuc.cl>
2024-04-19 00:53:18 -0400

    pagina

--- portafolio/aplicacion/urls.py
+++ portafolio/aplicacion/urls.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.urls import path
-from .views import pantalla
+from .views import pantalla,login,Solicitudes,Informes,Registro,ModificarU,CrearEjer,ModificarEjer,CrearDie,ModificarDie
 
 urlpatterns =[
-    path("",pantalla,name="algo")
+    path("",login,name="login"),
+    path("pantalla",pantalla,name="pantalla"),
+    
+    path("Solicitudes",Solicitudes,name="Solicitudes"),
+    path("Informes",Informes,name="Informes"),
+
+    path("Registro",Registro,name="Registro"),
+    path("ModificarU",ModificarU,name="ModificarU"),
+
+    path("CrearEjer",CrearEjer,name="CrearEjer"),
+    path("ModificarEjer",ModificarEjer,name="ModificarEjer"),
+
+    path("CrearDie",CrearDie,name="CrearDie"),
+    path("ModificarDie",ModificarDie,name="ModificarDie"),
 ]
--- portafolio/aplicacion/views.py
+++ portafolio/aplicacion/views.py
@@ -3,3 +3,30 @@ from django.shortcuts import render
 # Create your views here.
 def pantalla(request):
     return render(request,"aplicacion/inicio.html")
+
+def login(request):
+    return render(request,"aplicacion/login.html")
+
+def Solicitudes(request):
+    return render(request,"aplicacion/Solicitudes.html")
+
+def Informes(request):
+    return render(request,"aplicacion/Informes.html")
+
+def Registro(request):
+    return render(request,"aplicacion/RegistrarU.html")
+
+def ModificarU(request):
+    return render(request,"aplicacion/ModificarU.html")
+
+def CrearEjer(request):
+    return render(request,"aplicacion/CrearEje.html")
+
+def ModificarEjer(request):
+    return render(request,"aplicacion/ModificarEje.html")
+
+def CrearDie(request):
+    return render(request,"aplicacion/CrearDie.html")
+
+def ModificarDie(request):
+    return render(request,"aplicacion/ModificarDie.html")
