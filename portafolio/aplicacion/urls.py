04dc6daa314a9202f10fd4a21578bcad6852ec24
JonyRiverosF <jo.riverosf@duocuc.cl>
2024-04-21 00:56:39 -0400

    avance

--- portafolio/aplicacion/urls.py
+++ portafolio/aplicacion/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import pantalla,login,Solicitudes,Informes,Registro,ModificarU,CrearEjer,ModificarEjer,CrearDie,ModificarDie
+from .views import pantalla,login, Solicitudes,Informes, Registro,ListaUsu,ModificarU, CrearEjer,ListaEje,ModificarEjer, CrearDie,ListaDie,ModificarDie, CrearNot,ListaNot,ModificarNot
 
 urlpatterns =[
+
     path("",login,name="login"),
     path("pantalla",pantalla,name="pantalla"),
     
@@ -10,11 +11,19 @@ urlpatterns =[
     path("Informes",Informes,name="Informes"),
 
     path("Registro",Registro,name="Registro"),
+    path("ListaUsu",ListaUsu,name="ListaUsu"),
     path("ModificarU",ModificarU,name="ModificarU"),
 
     path("CrearEjer",CrearEjer,name="CrearEjer"),
+    path("ListaEje",ListaEje,name="ListaEje"),
     path("ModificarEjer",ModificarEjer,name="ModificarEjer"),
 
     path("CrearDie",CrearDie,name="CrearDie"),
+    path("ListaDie",ListaDie,name="ListaDie"),
     path("ModificarDie",ModificarDie,name="ModificarDie"),
-]
+
+    path("CrearNot",CrearNot,name="CrearNot"),
+    path("ListaNot",ListaNot,name="ListaNot"),
+    path("ModificarNot",ModificarNot,name="ModificarNot"),
+
+    ]
--- portafolio/aplicacion/views.py
+++ portafolio/aplicacion/views.py
@@ -7,26 +7,59 @@ def pantalla(request):
 def login(request):
     return render(request,"aplicacion/login.html")
 
+
+
+
 def Solicitudes(request):
     return render(request,"aplicacion/Solicitudes.html")
 
 def Informes(request):
     return render(request,"aplicacion/Informes.html")
 
+
+
+
 def Registro(request):
     return render(request,"aplicacion/RegistrarU.html")
+
+def ListaUsu(request):
+    return render(request,"aplicacion/ListaUsu.html")
 
 def ModificarU(request):
     return render(request,"aplicacion/ModificarU.html")
 
+
+
+
 def CrearEjer(request):
     return render(request,"aplicacion/CrearEje.html")
+
+def ListaEje(request):
+    return render(request,"aplicacion/ListaEje.html")
 
 def ModificarEjer(request):
     return render(request,"aplicacion/ModificarEje.html")
 
+
+
+
 def CrearDie(request):
     return render(request,"aplicacion/CrearDie.html")
 
+def ListaDie(request):
+    return render(request,"aplicacion/ListaDie.html")
+
 def ModificarDie(request):
     return render(request,"aplicacion/ModificarDie.html")
+
+
+
+
+def CrearNot(request):
+    return render(request,"aplicacion/CrearNot.html")
+
+def ListaNot(request):
+    return render(request,"aplicacion/ListaNot.html")
+
+def ModificarNot(request):
+    return render(request,"aplicacion/ModificarNot.html")
