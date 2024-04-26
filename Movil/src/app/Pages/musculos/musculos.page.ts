965751d2412989fca486398b48c7d4439bb19e0a
JonyRiverosF <jo.riverosf@duocuc.cl>
2024-04-26 01:07:43 -0400

    musculo/maquina

--- Movil/src/app/Pages/musculos/musculos.page.ts
+++ Movil/src/app/Pages/musculos/musculos.page.ts
@@ -8,8 +8,26 @@ import { Router } from '@angular/router';
 })
 export class MusculosPage implements OnInit {
 
+  IngreMusculos:boolean=true;
+  IngreMaquinas:boolean=false;
+
   constructor(private router: Router) { 
   }
+
+  IngresarMusculos(){
+    this.IngreMusculos=true;
+    this.IngreMaquinas=false;
+  }
+
+  IngresarMaquinas(){
+    this.IngreMaquinas=true;
+    this.IngreMusculos=false;
+  }
+
+
+
+
+
   
 
   irPerfil(){
